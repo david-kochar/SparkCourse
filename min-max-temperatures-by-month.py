aca0ee4f41d7ef08f544daadc383b20b678b9325
9AD589B1 <kocha017@gmail.com>
2020-05-10 19:47:23 -0500

    New files

--- min-max-temperatures-by-month.py
+++ min-max-temperatures-by-month.py
@@ -5,19 +5,18 @@ sc = SparkContext(conf = conf)
 
 def parseLine(line):
     fields = line.split(',')
-    stationIDDate = fields[0] + fields[1]
+    stationID = fields[0]
+    date      = str(fields[1])
     entryType = fields[2]
     temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
-    return (stationIDDate, entryType, temperature)
+    return (stationID, date, entryType, temperature)
 
 lines = sc.textFile("file:///SparkCourse/1800.csv")
 parsedLines = lines.map(parseLine)
-minTemps = parsedLines.filter(lambda x: x[1] in ["TMAX", "TMIN"]) #"PRCP" not in x[1]
-stationTemps = minTemps.map(lambda x: (x[0], x[2]))
-minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
-maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
-temps   = minTemps.join(maxTemps)
-results = temps.collect();
+allTemps = parsedLines.filter(lambda x: "PRCP" not in x[1])
+stationDailyTemps = allTemps.map(lambda x: ((x[0], x[1]), (x[3], x[3])))
+stationDailyMinMaxTemps = stationDailyTemps.reduceByKey(lambda x, y: (min(x[0], y[0]), max(x[1],y[1])))
+results = stationDailyMinMaxTemps.collect()
 
 for result in results:
-    print(result[0] + "\t{:.2f}F".format(result[1][0]) + "\t{:.2f}F".format(result[1][1]))
+    print(result[0][0] + "  " + result[0][1] + "\t{:.2f}F".format(result[1][0]) + "\t{:.2f}F".format(result[1][1]))
